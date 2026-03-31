3dcde5e81a9825b914024c5313f1b15e5d82c369
Rohit <flux.amv25@gmail.com>
2026-03-31 10:53:19 +0530

    Fixed custom decorator , utils for refreshToken , common @Cookie decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,26 +1,25 @@
-import { Body, Controller, Get, Post, Request, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from 'src/auth/dto/registerUser.dto';
 import { LoginDto } from './dto/loginUser.dto';
 import { JwtAuthGuard } from './guards';
-import { UserService } from 'src/user/user.service';
-import { JwtService } from '@nestjs/jwt';
-import { Cookies } from 'src/user/userCoookie.decorator';
 import type { Response } from 'express';
+import { GetUser } from './decorators';
+import type { JwtUser } from './types';
+import { setRefreshToken } from './utils/set-refresh-token';
+import { RefreshToken } from 'src/common/decorators';
 
 @Controller('auth')
 export class AuthController {
 
-    constructor(private readonly authService:AuthService, private readonly userService: UserService,private readonly jwtService:JwtService){}
-
+    constructor(private readonly authService:AuthService){}
+    @HttpCode(201)
     @Post('register')
     async register(@Body() registerUserDto:RegisterDto, @Res({ passthrough : true }) res:Response){
         const tokens = await this.authService.registerUser(registerUserDto);
-        res.cookie('refreshToken',tokens.refreshToken, {
-            httpOnly : true,
-            secure: true,
-            sameSite: 'strict',
-        })
+        
+        setRefreshToken(res,tokens.refreshToken);
+
         return {
             accessToken : tokens.accessToken
         }
@@ -29,11 +28,9 @@ export class AuthController {
     @Post('login')
     async login(@Body() LoginDto:LoginDto,@Res({ passthrough : true }) res:Response){
         const tokens = await this.authService.loginUser(LoginDto);
-        res.cookie('refreshToken',tokens.refreshToken, {
-            httpOnly : true,
-            secure: true,
-            sameSite: 'strict',
-        })
+        
+        setRefreshToken(res,tokens.refreshToken);
+
         return {
             accessToken : tokens.accessToken
         }
@@ -41,16 +38,12 @@ export class AuthController {
 
     @Post('refresh')
     async newAccessToken(
-        @Cookies('refreshToken') refreshToken: string,
+        @RefreshToken() refreshToken: string,
         @Res({ passthrough : true }) res: Response,
     ){
         const tokens = await this.authService.reAccessToken(refreshToken);
 
-        res.cookie('refreshToken',tokens.refreshToken, {
-            httpOnly : true,
-            secure: true,
-            sameSite: 'strict',
-        })
+        setRefreshToken(res,tokens.refreshToken);
 
         return {
             accessToken : tokens.accessToken
@@ -59,10 +52,10 @@ export class AuthController {
 
     @UseGuards(JwtAuthGuard)
     @Get('profile')
-    async getProfile(@Request() req) {
+    // why we imported JwtUser as type is we need it as a type during runtime before it was crashing like interface JwtUser {}
+    async getProfile(@GetUser() user:JwtUser) {
         return {
-            data : req.user
+            data : user
         };
     }
-
 }
