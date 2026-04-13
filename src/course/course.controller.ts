0375cc4da39bedf20e8ca37c863030ef08c3ffbe
Rohit <flux.amv25@gmail.com>
2026-04-13 07:19:48 +0530

    fix: course controller cleaned

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,40 +1,19 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { CreateCourseDto } from './dto/create-course.dto';
-import { UpdateCourseDto } from './dto/update-course.dto';
-import { JwtAuthGuard, RoleGuard } from 'src/auth/guards';
 import { Roles } from 'src/auth/decorators';
 import { Role } from '@prisma/client';
+import { AtGuard, RoleGuard } from 'src/auth/guards';
 
 @Controller('courses')
 export class CourseController {
   constructor(private readonly courseService: CourseService) {}
 
-  @UseGuards(JwtAuthGuard, RoleGuard)
+  @UseGuards(AtGuard, RoleGuard)
   @Roles(Role.ADMIN)
   @Post('create')
   create(@Body() createCourseDto: CreateCourseDto, @Req() req) {
     const userId = req.user.userId;
     return this.courseService.create(createCourseDto,userId);
-  }
-
-  @Get('get')
-  findAll() {
-    return this.courseService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.courseService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
-    return this.courseService.update(+id, updateCourseDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.courseService.remove(+id);
   }
 }
