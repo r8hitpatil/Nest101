62afceb88074b0b10e254820eeda192766df76fa
Rohit <flux.amv25@gmail.com>
2026-04-13 07:12:15 +0530

    added Refresh token guard

---
+++ src/auth/guards/rt-auth.guard.ts
@@ -0,0 +1,9 @@
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class RtGuard extends AuthGuard('jwt-refresh') {
+  constructor() {
+    super();
+  }
+}
