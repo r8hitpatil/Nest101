0856fef191c39123b248b832594da0c53b740067
Rohit <flux.amv25@gmail.com>
2026-04-13 07:14:01 +0530

    added new auth strategies

---
+++ src/auth/strategies/at.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import type { Request } from "express";
+import { Strategy } from "passport-jwt";
+import { JwtPayload } from "../types";
+
+@Injectable()
+export class AtStrategy extends PassportStrategy(Strategy,'at-jwt'){
+    constructor(config: ConfigService){
+        super({
+            jwtFromRequest : (req:Request) => {
+                let token = null;
+                if(req && req.cookies){
+                    token = req.cookies['access_token'];
+                }
+                return token;
+            },
+            secretOrKey : config.getOrThrow<string>('ACCESS_SECRET'),
+        })
+    }
+
+    validate(payload:JwtPayload){
+        return {
+            userId:payload.sub,
+            email:payload.email,
+            role:payload.role
+        };
+    }
+}
